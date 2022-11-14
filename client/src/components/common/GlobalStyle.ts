3877d734ee0754553d740a45fd2769dcf2856fca
sooyeon73 <sooy8951@gmail.com>
2022-11-14 16:59:01 +0900

    style: var color 등록

--- client/src/components/common/GlobalStyle.ts
+++ client/src/components/common/GlobalStyle.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   body {
-    background: linear-gradient(#99B1DB 55%, #ffffff 50%);
+    background: linear-gradient(var(--color-main) 55%, #ffffff 50%);
     height: 100vh;
     place-items: center;
     display:grid;
