efffb1c8818cac04413371deb260a95fe7dfb9e9
ParkKyungChan <nd098pkc@naver.com>
2022-11-15 10:00:29 +0900

    feat: 친구목록 노출 Bar 구현
    친구 리스트를 프로필 사진으로 보여주는 Bar를 구현했습니다. 친구를 추가할 수 있는 plus버튼은 UI만 우선 구현해두었습니다.

---
+++ client/src/components/MainPage/FriendsBar/style.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const FriendsBarContainer = styled.div`
+  width: 90rem;
+  height: 3.4rem;
+  background: white;
+  border-radius: 0.5rem;
+  margin: 0 auto;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  transform: translate(0, -100px);
+`;
+
+export const ProfileBox = styled.div<{
+  imgURL: string;
+}>`
+  width: 3.4rem;
+  height: 3.4rem;
+  border-radius: 3.4rem;
+  border: 1px solid #d4d4d4;
+  margin: 0 0.5rem;
+  box-sizing: border-box;
+  background: url(${(props) => props.imgURL}) no-repeat center center / ${(props) => (props.imgURL === './plus.svg' ? '1.25rem 2.8rem' : '3.5rem 3.5rem')};
+  &:hover {
+    border: 5px solid #bccdec;
+  }
+`;
--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import GNB from '../components/MainPage/TopBar';
+import FriendsBar from '../components/MainPage/FriendsBar';
 
 const MainPage = () => {
-  return <GNB />;
+  return (
+    <>
+      <GNB />
+      <FriendsBar />
+    </>
+  );
 };
 
 export default MainPage;
