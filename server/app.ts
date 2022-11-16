6567ef0ae7b0906b66d213e7b5feb442b6c3fed4
mikaniz <k515jina@gmail.com>
2022-11-16 18:37:49 +0900

    feat: express 정적 파일 위치 지정

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import { CORS_ORIGIN, port, version } from './src/constants';
 import apiRouter from './src/api/index';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 
@@ -13,6 +14,7 @@ const corsOptions = {
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(`/api/${version}`, apiRouter);
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(port, () => {
   console.log(`app listening to port ${port}`);
