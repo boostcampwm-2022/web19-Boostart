2bea7f2b59d0170bd2c61d565e6433c86af6f31b
hhyuw <high2092@gmail.com>
2022-11-21 13:41:52 +0900

    feat: label 생성 API

--- server/src/api/label.ts
+++ server/src/api/label.ts
@@ -11,4 +11,11 @@ router.get('/', authenticateToken, async (req: AuthorizedRequest, res) => {
   res.json(labels);
 });
 
+router.post('/', authenticateToken, async (req: AuthorizedRequest, res) => {
+  const { userIdx } = req.user;
+  const { title, color, unit } = req.body;
+  await executeSql('insert into label (title, color, unit, user_idx) values (?, ?, ?, ?)', [title, color, unit, userIdx]);
+  res.sendStatus(200);
+});
+
 export default router;
