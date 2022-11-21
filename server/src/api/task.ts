6ad461d25f4dd0255fa4823769b68e1d61575f71
hhyuw <high2092@gmail.com>
2022-11-21 14:44:08 +0900

    feat: task 생성 API 보완 Request에 필요한 필드 추가

--- server/src/api/task.ts
+++ server/src/api/task.ts
@@ -13,9 +13,29 @@ router.get('/', authenticateToken, async (req: AuthorizedRequest, res) => {
 
 router.post('/', authenticateToken, async (req: AuthorizedRequest, res) => {
   const { userIdx } = req.user;
-  const { title, importance, startedAt, endedAt, location, isPublic, tagIdx, labels } = req.body;
-  const result = await executeSql('insert into task (title, importance, public, user_idx) values (?, ?, ?, ?)', [title, importance, isPublic, userIdx]);
-  res.send(200);
+  const { title, importance, startedAt, endedAt, lat, lng, isPublic, tagIdx, content, done, date, labels } = req.body;
+  const result = await executeSql('insert into task (title, importance, date, started_at, ended_at, lat, lng, content, done, public, tag_idx, user_idx) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [
+    title,
+    importance,
+    date,
+    startedAt,
+    endedAt,
+    lat,
+    lng,
+    content,
+    done,
+    isPublic,
+    tagIdx,
+    userIdx,
+  ]);
+
+  const taskIdx = result.insertId;
+  await Promise.all(
+    labels.map(({ labelIdx, amount }) => {
+      executeSql('insert into task_label (task_idx, label_idx, amount) value (?, ?, ?)', [taskIdx, labelIdx, amount]);
+    })
+  );
+  res.sendStatus(200);
 });
 
 export default router;
