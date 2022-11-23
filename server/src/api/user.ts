85fb067f343b9e2c4fdd7d0f2d781c177a981a3a
mikaniz <k515jina@gmail.com>
2022-11-23 13:17:27 +0900

    feat: DB error 처리 추가

--- server/src/api/user.ts
+++ server/src/api/user.ts
@@ -8,8 +8,12 @@ const router = Router();
 router.get('/', authenticateToken, async (req: AuthorizedRequest, res) => {
   const { userIdx } = req.user;
   const userId = `%${req.query.user_id}%`;
-  const users = await executeSql('select idx, user_id, username, profile_img from user where user_id LIKE ? and idx != ?', [userId, userIdx.toString()]);
-  res.json(users);
+  try {
+    const users = await executeSql('select idx, user_id, username, profile_img from user where user_id LIKE ? and idx != ?', [userId, userIdx.toString()]);
+    res.json(users);
+  } catch {
+    res.sendStatus(500);
+  }
 });
 
 export default router;
