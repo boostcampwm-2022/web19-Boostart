759ed15b2392a2dbd97635c36f6a29396e0e5cb8
mikaniz <k515jina@gmail.com>
2022-11-23 13:19:12 +0900

    feat: DB error 처리 추가

--- server/src/api/friend.ts
+++ server/src/api/friend.ts
@@ -7,12 +7,16 @@ const router = Router();
 
 router.get('/', authenticateToken, async (req: AuthorizedRequest, res) => {
   const { userIdx } = req.user;
-  const users = await executeSql('select idx, user_id, username, profile_img from user inner join friendship on idx = sender_idx or idx = receiver_idx where idx != ? and (receiver_idx = ? or sender_idx = ?) and accepted = true', [
-    userIdx.toString(),
-    userIdx.toString(),
-    userIdx.toString(),
-  ]);
-  res.json(users);
+  try {
+    const users = await executeSql('select idx, user_id, username, profile_img from user inner join friendship on idx = sender_idx or idx = receiver_idx where idx != ? and (receiver_idx = ? or sender_idx = ?) and accepted = true', [
+      userIdx.toString(),
+      userIdx.toString(),
+      userIdx.toString(),
+    ]);
+    res.json(users);
+  } catch {
+    res.sendStatus(500);
+  }
 });
 
 export default router;
