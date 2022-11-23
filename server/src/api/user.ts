4da6ae46179f1b0caf214901e181a663b8506f7f
mikaniz <k515jina@gmail.com>
2022-11-23 09:59:53 +0900

    feat: 본인을 검색 결과에서 제외하도록 수정

--- server/src/api/user.ts
+++ server/src/api/user.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { executeSql } from '../db';
+import { AuthorizedRequest } from '../types';
 import { authenticateToken } from '../utils/auth';
 
 const router = Router();
 
-router.get('/', authenticateToken, async (req, res) => {
+router.get('/', authenticateToken, async (req: AuthorizedRequest, res) => {
+  const { userIdx } = req.user;
   const userId = `%${req.query.user_id}%`;
-  const users = await executeSql('select idx, user_id, username, profile_img from user where user_id LIKE ?', [userId]);
+  const users = await executeSql('select idx, user_id, username, profile_img from user where user_id LIKE ? and idx != ?', [userId, userIdx.toString()]);
   res.json(users);
 });
 
