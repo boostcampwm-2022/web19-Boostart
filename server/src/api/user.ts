8ec7d13e19bb504063aa92e58e28e3221b2a077a
mikaniz <k515jina@gmail.com>
2022-11-28 16:30:58 +0900

    fix: API response의 key 값을 camelCase 형태로 수정

--- server/src/api/user.ts
+++ server/src/api/user.ts
@@ -10,7 +10,7 @@ router.get('/', authenticateToken, async (req: AuthorizedRequest, res) => {
   const { userIdx } = req.user;
   const userId = `%${req.query.user_id}%`;
   try {
-    const users = await executeSql('select idx, user_id, username, profile_img from user where user_id LIKE ? and idx != ?', [userId, userIdx]);
+    const users = await executeSql('select idx, user_id as userId, username, profile_img as profileImg from user where user_id LIKE ? and idx != ?', [userId, userIdx]);
     res.json(users);
   } catch {
     res.sendStatus(500);
@@ -20,7 +20,7 @@ router.get('/', authenticateToken, async (req: AuthorizedRequest, res) => {
 router.get('/me', authenticateToken, async (req: AuthorizedRequest, res) => {
   const { userIdx } = req.user;
   try {
-    const [userInfo] = (await executeSql('select user_id, username, profile_img from user where idx = ?', [userIdx])) as RowDataPacket[];
+    const [userInfo] = (await executeSql('select user_id as userId, username, profile_img as profileImg from user where idx = ?', [userIdx])) as RowDataPacket[];
     res.json(userInfo);
   } catch {
     res.sendStatus(500);
