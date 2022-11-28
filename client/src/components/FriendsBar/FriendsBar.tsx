7fa5ca7bbf58205810923af401ffe4a312887bde
ParkKyungChan <nd098pkc@naver.com>
2022-11-28 17:55:21 +0900

    feat: 친구창 제일 앞부분 자신의 프로필사진 확인 가능

--- client/src/components/FriendsBar/FriendsBar.tsx
+++ client/src/components/FriendsBar/FriendsBar.tsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 import * as S from './FriendsBar.style';
 
 const FriendsBar = () => {
+  const [myProfile, setMyProfile] = useState<FriendsList | null>(null);
   const [friendsList, setFriendsList] = useState<FriendsList[] | null>(null);
   const plusIcon = '/plus.svg';
   const getFriendsList = async () => {
@@ -16,14 +17,24 @@ const FriendsBar = () => {
       console.log(error);
     }
   };
+  const getMyProfile = async () => {
+    try {
+      const response = await axios.get(`${HOST}/api/v1/user/me`);
+      setMyProfile(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     getFriendsList();
+    getMyProfile();
   }, []);
 
   return (
     <>
       <S.FriendsBarContainer>
+        {myProfile && <S.ProfileBox imgURL={myProfile.profileImg}></S.ProfileBox>}
         {friendsList &&
           friendsList.map(({ idx, userId, profileImg }) => {
             return <S.ProfileBox key={userId} data-idx={idx} imgURL={profileImg}></S.ProfileBox>;
