cbb3ec27384393018208f537444fc7407396361b
sooyeon73 <sooy8951@gmail.com>
2022-11-30 17:20:37 +0900

    design: 불필요한 스크롤 제거 및 스크롤 디자인 수정

--- client/src/components/MainContainer/MainContainer.style.ts
+++ client/src/components/MainContainer/MainContainer.style.ts
@@ -19,19 +19,3 @@ export const LeftSection = styled.div`
 export const RightSection = styled.div`
   width: 44.7rem;
 `;
-
-export const NewTaskButton = styled.div`
-  cursor: pointer;
-  position: relative;
-  text-align: center;
-  color: white;
-  font-size: 1.6rem;
-  line-height: 2.9rem;
-  left: 40rem;
-  bottom: 5.5rem;
-  width: 3.1rem;
-  height: 3.1rem;
-  border-radius: 20rem;
-  background-color: var(--color-main);
-  box-shadow: 0px 0px 5px 3px rgba(175, 175, 175, 0.2);
-`;
