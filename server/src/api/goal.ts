9d38c14f832fe1f3f759f70d040790d2f6baa182
mikaniz <k515jina@gmail.com>
2022-12-05 18:48:18 +0900

    fix: currentAmount가 0인 목표가 표시되지 않던 버그 수정

--- server/src/api/goal.ts
+++ server/src/api/goal.ts
@@ -12,8 +12,8 @@ router.get('/', authenticateToken, async (req: AuthorizedRequest, res) => {
   if (!date) return res.status(400).send({ msg: '날짜를 지정해주세요.' });
   try {
     const goals = (await executeSql(
-      'select goal.idx, goal.title, goal.label_idx as labelIdx, goal.amount as goalAmount, sum(task_label.amount) as currentAmount, goal.over from goal inner join task_label on goal.label_idx = task_label.label_idx inner join task on task_label.task_idx = task.idx where goal.user_idx = ? and task.date = ? group by goal.idx',
-      [userIdx, date]
+      'select idx, title, goal.label_idx as labelIdx, goal.amount as goalAmount, ifnull(sum(task_label.amount), 0) as currentAmount, over from goal left join (select label_idx, amount from task_label inner join task on task_label.task_idx = task.idx where user_idx = ? and date = ?) task_label on goal.label_idx = task_label.label_idx where user_idx = ? and date = ? group by idx',
+      [userIdx, date, userIdx, date]
     )) as RowDataPacket;
     res.json(goals);
   } catch {
