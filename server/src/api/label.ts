0d1caf7d71e9341176836931e81874efa499c92c
hhyuw <high2092@gmail.com>
2022-12-06 13:34:29 +0900

    fix: 라벨 생성 API 입력값 검증 추가

--- server/src/api/label.ts
+++ server/src/api/label.ts
@@ -6,6 +6,37 @@ import { authenticateToken } from '../utils/auth';
 
 const router = Router();
 
+const LabelBodyKeys = {
+  title: 'title',
+  color: 'color',
+  unit: 'unit',
+} as const;
+
+type LabelBodyKeys = typeof LabelBodyKeys[keyof typeof LabelBodyKeys];
+
+const validate = (key: LabelBodyKeys, value: string) => {
+  switch (key) {
+    case LabelBodyKeys.title: {
+      if (!(value && typeof value === 'string')) throw new ValidationError('올바른 제목을 입력해주세요.');
+      return true;
+    }
+    case LabelBodyKeys.unit: {
+      if (!(value && typeof value === 'string')) throw new ValidationError('올바른 단위를 입력해주세요.');
+      return true;
+    }
+    case LabelBodyKeys.color: {
+      const regex = /^#[A-Fa-f0-9]{6}$/;
+      if (!regex.test(value)) throw new ValidationError('올바른 색상을 입력해주세요.');
+      return true;
+    }
+    default: {
+      throw new ValidationError('잘못된 정보가 입력되었어요.');
+    }
+  }
+};
+
+class ValidationError extends Error {}
+
 router.get('/', authenticateToken, async (req: AuthorizedRequest, res) => {
   const { userIdx } = req.user;
   try {
@@ -20,7 +51,17 @@ router.get('/', authenticateToken, async (req: AuthorizedRequest, res) => {
 
 router.post('/', authenticateToken, async (req: AuthorizedRequest, res) => {
   const { userIdx } = req.user;
+
+  try {
+    Object.values(LabelBodyKeys).forEach((key) => {
+      validate(key, req.body[key]);
+    });
+  } catch (error) {
+    return res.status(400).json({ msg: error.message });
+  }
+
   const { title, color, unit } = req.body;
+
   try {
     const existLabel = ((await executeSql('select idx from label where user_idx = ? and title = ?', [userIdx, title])) as RowDataPacket).length > 0;
     if (existLabel) return res.status(409).json({ msg: '이미 존재하는 라벨이에요.' });
