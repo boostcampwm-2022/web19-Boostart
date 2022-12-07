4733b335840668bb437fbb59dd0adc687ee5e41f
mikaniz <k515jina@gmail.com>
2022-12-07 12:49:39 +0900

    feat: calendar route 설정

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -7,6 +7,7 @@ import userRouter from './user';
 import friendRouter from './friend';
 import emoticonRouter from './emoticon';
 import goalRouter from './goal';
+import calendarRouter from './calendar';
 
 const router = express.Router();
 
@@ -18,5 +19,6 @@ router.use('/user', userRouter);
 router.use('/friend', friendRouter);
 router.use('/emoticon', emoticonRouter);
 router.use('/goal', goalRouter);
+router.use('/calendar', calendarRouter);
 
 export default router;
