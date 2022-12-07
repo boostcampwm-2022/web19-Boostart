2a36d090dc2d2857ddcef9d46f62272490db3dfd
hhyuw <high2092@gmail.com>
2022-12-07 19:09:01 +0900

    fix: 목표 탭 - 라벨 색상 변경 API 주소가 잘못되어 있던 점 수정

--- client/src/components/MainContainer/GoalManager.tsx
+++ client/src/components/MainContainer/GoalManager.tsx
@@ -64,8 +64,8 @@ const httpDeleteLabel = async (idx: number) => {
   return response;
 };
 
-const httpPatchLabel = async ({ title, color }: { title?: string; color?: string }) => {
-  const response = await axios.post(`${HOST}/api/v1/label/color`, { title, color });
+const httpPatchLabel = async (idx: number, { title, color }: { title?: string; color?: string }) => {
+  const response = await axios.patch(`${HOST}/api/v1/label/${idx}`, { title, color });
   return response;
 };
 
@@ -163,8 +163,14 @@ const GoalModal = ({ isLabelModalOpen, setIsLabelModalOpen, handleCloseButtonCli
   };
 
   const handleColorInputBlur = async () => {
+    if (!selectedLabelIndex) return;
+    if (!color) return;
     try {
-      httpPatchLabel({ color });
+      httpPatchLabel(selectedLabelIndex, { color });
+      const label = labelList.find((label) => label.idx === selectedLabelIndex);
+      if (!label) return;
+      label.color = color;
+      setLabelList([...labelList]);
     } catch (error) {
       console.log(error);
     }
@@ -268,7 +274,7 @@ const LabelModal = ({ handleCloseButtonClick }: LabelModalProps) => {
         <S.VertialRule />
         <S.LabelModalLabelColorInput value={color} type="color" {...register('color')} onChange={handleLabelColorChange} />
       </S.LabelModalLabel>
-      <S.LabelModalLabelCreateButton>ADD LABEL</S.LabelModalLabelCreateButton>
+      <S.LabelModalLabelCreateButton>ADD LABEL!</S.LabelModalLabelCreateButton>
     </S.LabelModal>
   );
 };
