e9b947c5a7aa5cdbbf0817de06f2fce613f1bfdc
sooyeon73 <sooy8951@gmail.com>
2022-12-08 13:54:49 +0900

    fix: url 변경 시 메뉴 셀렉터가 변경되지 않는 문제  해결

--- client/src/components/MainContainer/MenuSelector.tsx
+++ client/src/components/MainContainer/MenuSelector.tsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Menus, RoutePath } from '../../constants';
+import { menuState } from '../common/atoms';
+import { useRecoilState } from 'recoil';
 import * as S from './Calendar.style';
 
 const MenuSelector = () => {
-  const [selectedMenu, setSelectedMenu] = useState('LOG');
+  const [currentMenu, setCurrentMenu] = useRecoilState(menuState);
+
   const navigate = useNavigate();
   const handleMenuClick = (e: React.MouseEvent) => {
     const target = e.target;
     if (!(target instanceof HTMLDivElement)) return;
     const selectedMenu = target.dataset.menu;
     if (selectedMenu) {
-      setSelectedMenu(selectedMenu);
       navigate(RoutePath[selectedMenu]);
     }
   };
@@ -20,7 +22,7 @@ const MenuSelector = () => {
     <S.MenuSelector>
       {Menus.map((menu) => {
         return (
-          <S.MenuButton data-menu={menu} onClick={handleMenuClick} isActivatedMenu={menu === selectedMenu}>
+          <S.MenuButton data-menu={menu} onClick={handleMenuClick} isActivatedMenu={menu === currentMenu}>
             {menu}
           </S.MenuButton>
         );
