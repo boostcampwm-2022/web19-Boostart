36735101df7bec8a5831a013f6ba2c8b7962b093
sooyeon73 <sooy8951@gmail.com>
2022-12-12 20:32:06 +0900

    feat: DIARY / MAP SubContainer로 공통화 / css 조정

--- client/src/components/MainContainer/Diary.style.ts
+++ client/src/components/MainContainer/Diary.style.ts
@@ -1,27 +1,5 @@
 import styled from 'styled-components';
 
-export const DiaryTitle = styled.span`
-  display: inline-block;
-  color: white;
-  font-size: 1.7rem;
-  font-family: 'Press Start 2P', cursive;
-  transform: translate(1.75rem, 0.43rem);
-  z-index: 1;
-  span {
-    font-size: 1.2rem;
-  }
-`;
-
-export const Container = styled.div`
-  height: 36rem;
-  background: white;
-  border-radius: 1rem;
-  margin-top: 0rem;
-  margin-bottom: 1rem;
-  padding: 0.5rem;
-  box-shadow: 0px 0px 10px 5px rgba(175, 175, 175, 0.25);
-`;
-
 export const DiaryContainer = styled.div`
   position: relative;
   user-select: none;
@@ -43,12 +21,5 @@ export const DiaryNavBarSection = styled.div`
   align-items: flex-end;
   grid-area: nav;
   box-sizing: border-box;
-  cursor: default;
-`;
-
-export const DateController = styled.span`
-  color: #99b1db;
-  font-size: 1.5rem;
-  font-family: 'Press Start 2P', cursive;
   cursor: default;
 `;
